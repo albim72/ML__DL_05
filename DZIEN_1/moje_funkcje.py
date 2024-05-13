9d8ceeb31161e0088bd4a15925b947cd88637e7c
albim72 <marcin.albiniak72@gmail.com>
2024-05-13 10:28:20 +0200

    Update moje_funkcje.py

--- DZIEN_1/moje_funkcje.py
+++ DZIEN_1/moje_funkcje.py
@@ -14,3 +14,21 @@ print(policz(2.2,3,0.45,4))
 print(policz(56,True,0.005,-3))
 print(policz(3,8,3.5))
 print(n)
+
+#przykład 2 - funkcje anonimowe
+
+print((lambda a:a+16)(17))
+s = lambda a,b,z=3:(a*b-2*z)/(a+b+z)
+print(s(2,5,7))
+print(s(2,5))
+
+num = [56,3,63,-64,0,210,432,2132,-34,-453,333,0,190,267]
+
+#zadanie - -utwórz listę nbparzyste i przekaż do niej elementy list num o wartości parzystej
+
+nbparzyste = list(filter(lambda x:x%2==0,num))
+print(nbparzyste)
+
+#utwórz nową listę cube i przekaż do niej elementy list num podmniesione do potęgi trzeciej
+cube = list(map(lambda x:x**3,num))
+print(cube)
